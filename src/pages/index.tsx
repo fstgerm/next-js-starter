03123f5bf1bb83d6d8a2624a01ddbe240fff54e8
François St-Germain <fstgerm@gmail.com>
2022-10-15 17:58:36 -0400

    chore: add base linter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <div className="text-red-500">home</div>;
+  return <div className="text-3xl text-red-500">home</div>;
 };
 
 export default Home;
