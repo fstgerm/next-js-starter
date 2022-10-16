615444245e52654f1fdd5e3386d75267514cc326
François St-Germain <fstgerm@gmail.com>
2022-10-15 21:27:38 -0400

    fix(storybook): enable images

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,30 +1,10 @@
-// module.exports = {
-//   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-//   addons: [
-//     '@storybook/addon-links',
-//     '@storybook/addon-essentials',
-//     '@storybook/addon-interactions',
-//   ],
-//   framework: '@storybook/react',
-//   core: {
-//     builder: '@storybook/builder-webpack5',
-//   },
-// };
-
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
+    'storybook-addon-next',
   ],
   framework: '@storybook/react',
   core: {
